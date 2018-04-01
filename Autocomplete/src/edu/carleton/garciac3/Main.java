443d3039796a2b11823f3c90214ead7455bd293a
Joey Cook-Gallardo <cookj3@carleton.edu>
2018-04-01 14:30:26 -0500

    Changed text

--- Autocomplete/src/edu/carleton/garciac3/Main.java
+++ Autocomplete/src/edu/carleton/garciac3/Main.java
@@ -3,6 +3,6 @@ package edu.carleton.garciac3;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+        System.out.println("I don't know what I am doing");
     }
 }
