a12a958c77f7bc40ae5f6b324bb108c7b0d52a7f
CGarcia346 <garciac3@carleton.edu>
2018-05-15 23:42:52 -0500

    updated static

--- webapp/static/MTG.js
+++ webapp/static/MTG.js
@@ -18,6 +18,10 @@ function initialize() {
     if (element){
         getRandom();
     }
+		element = document.getElementById('card_table');
+		if (element){
+        getCard();
+    }
 }
 
 function getBaseURL() {
@@ -37,15 +41,15 @@ function onSetsButtonClicked(){
 
         // Build the table body.
         var tableBody = '';
-        
+
         tableBody = '<th> Name </th>';
         tableBody += '<th> Release Date </th>';
         tableBody += '<th> Border </th>';
-        
+
         for (var k = 0; k < setsList.length; k++) {
-       
+
             tableBody += '<tr>';
-            
+
             tableBody += '<td>' +setsList[k]['name'];
             tableBody += '<td>' +setsList[k]['releaseDate'];
             tableBody += '<td>' +setsList[k]['border'];
@@ -61,7 +65,7 @@ function onSetsButtonClicked(){
         }
 
         var displayValue = resultsTableElement.style.display;
-        
+
     })
 
     // Log the error if anything went wrong during the fetch.
@@ -75,7 +79,7 @@ function getSets() {
     // repeating those comments here. Read through this code
     // and see if it makes sense to you.
     var url = getBaseURL() + '/sets';
-    
+
     fetch(url, {method: 'get'})
 
     .then((response) => response.json())
@@ -129,7 +133,7 @@ function getArtists() {
     .catch(function(error) {
         console.log(error);
     });
-}   
+}
 function getRandom() {
     var url = getBaseURL() + '/random';
     fetch(url, {method: 'get'})
@@ -138,7 +142,6 @@ function getRandom() {
         var tableBody = '<tr>' + '<td>' + 'Name' + '</td>' + '<td>' +card['name'] + '</td>' + '</tr>';
         tableBody += '<tr>' + '<td>' + 'Colors' + '</td>' + '<td>' + card['colors'] + '</td>' + '</tr>';
         tableBody += '<tr>' + '<td>' + 'Color Identity' + '</td>' + '<td>' + card['colorIdentity'] + '</td>' +  '</tr>';
-
         tableBody += '<tr>' + '<td>' + 'Manacost' + '</td>' + '<td>' +  card['manaCost'] + '</td>' + '</tr>';
         tableBody += '<tr>' + '<td>' + 'CMC' + '</td>' + '<td>' + card['cmc'] + '</td>' + '</tr>';
         tableBody += '<tr>' + '<td>' + 'Type' + '</td>' + '<td>' + card['type'] + '</td>' + '</tr>';
@@ -150,6 +153,45 @@ function getRandom() {
         tableBody+= '<tr>' + '<td>' + 'Flavor Text' + '</td>' + '<td>' + card['flavor'] + '</td>' + '</tr>';
         tableBody += '<tr>' + '<td>' + 'Artist' + '</td>' + '<td>' + card['artist'] + '</td>' + '</tr>';
         var randomTable = document.getElementById('random_table');
+        if(randomTable){
+            randomTable.innerHTML = tableBody;
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
+
+function getCard() {
+    var url = getBaseURL() + '/card?';
+		var filter = document.getElementById('search');
+		url = url + filter;
+		var searchable = filter.replace("name=", "");
+
+    fetch(url, {method: 'get'})
+    .then((response) => response.json())
+    .then(function(cards){
+
+				for( var i = 0; i < cards.length; i++){
+					if(cards['name'] == searchable){
+						var tableBody = '<tr>' + '<td>' + 'Name' + '</td>' + '<td>' +card['name'] + '</td>' + '</tr>';
+						tableBody += '<tr>' + '<td>' + 'Colors' + '</td>' + '<td>' + card['colors'] + '</td>' + '</tr>';
+						tableBody += '<tr>' + '<td>' + 'Color Identity' + '</td>' + '<td>' + card['colorIdentity'] + '</td>' +  '</tr>';
+						tableBody += '<tr>' + '<td>' + 'Manacost' + '</td>' + '<td>' +  card['manaCost'] + '</td>' + '</tr>';
+						tableBody += '<tr>' + '<td>' + 'CMC' + '</td>' + '<td>' + card['cmc'] + '</td>' + '</tr>';
+						tableBody += '<tr>' + '<td>' + 'Type' + '</td>' + '<td>' + card['type'] + '</td>' + '</tr>';
+						tableBody += '<tr>' + '<td>' + 'Types' + '</td>' +'<td>'+ card['types'] + '</td>' + '</tr>';
+						tableBody += '<tr>' + '<td>' + 'Subtypes' + '</td>' + '<td>' + card['subtypes'] + '</td>' + '</tr>';
+						tableBody+= '<tr>' + '<td>' + 'Text' + '</td>' + '<td>' + card['text'] + '</td>' + '</tr>';
+						tableBody+= '<tr>' + '<td>' + 'Power' + '</td>' + '<td>' + card['power'] + '</td>' + '</tr>';
+						tableBody+= '<tr>' + '<td>' + 'Toughness' + '</td>' + '<td>' + card['toughness'] + '</td>' + '</tr>';
+						tableBody+= '<tr>' + '<td>' + 'Flavor Text' + '</td>' + '<td>' + card['flavor'] + '</td>' + '</tr>';
+						tableBody += '<tr>' + '<td>' + 'Artist' + '</td>' + '<td>' + card['artist'] + '</td>' + '</tr>';
+						break;
+					}
+				}
+
+        var randomTable = document.getElementById('card_table');
         if(randomTable){
             randomTable.innerHTML = tableBody;
         }
