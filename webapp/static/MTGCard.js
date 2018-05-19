5370ada3523eb8adf4c47b773b2e1752b7d601ca
CGarcia346 <garciac3@perlman.mathcs.carleton.edu>
2018-05-19 15:07:14 -0500

    Joey never does any work

--- webapp/static/MTGCard.js
+++ webapp/static/MTGCard.js
@@ -7,32 +7,62 @@ function initialize() {
     var searchText = url.searchParams.get('name');
     var searchname = document.getElementById('search');
     var url = getBaseURL() + '/cards?name=' + searchText;
-    var setName = getBaseURL() + '/sets'
+
     fetch(url, {method: 'get'})
     .then((response) => response.json())
     .then(function(cards){
 
-    fetch(setName, {method: 'get'})
-    var setList() = .then((response) => response.json())
+    var head = '<th>' + 'Set' + '</th>'
+    head += '<th>' + 'Card Name' + '</th>'
+    head += '<th>' + 'Artist' + '</th>'
+    head += '<th>' + 'Text' + '</th>'
+    head += '<th>' + 'Flavor Text' + '</th>'
+    var tableBody = ""
+    var subBody = ""
+	var priority = ""
+    for( var i = 0; i < cards.length; i++){
+        var setid = cards[i]['set_id']
+        var name = cards[i]['name']
+        var artist = cards[i]['artist']
+        var text = cards[i]['text']
+        var flavor = cards[i]['flavor']
+        var link = '<a href= /set/' +setid + '>' + setid + '</a>'
+                if(searchText == name){
+					tableBody += '<tr>'
+                    tableBody += '<td>' + link + '</td>'
+					tableBody += '<td>' + name + '</td>'
+                    tableBody += '<td>' + artist + '</td>'
+                    tableBody += '<td>' + text + '</td>'
+                    tableBody += '<td>' + flavor + '</td>'
+					tableBody += '</tr>'
+                }
+                else{
+                    
+                    if(cards[i]['name'].startsWith(searchText)){
+                        priority += '<tr>'
+                        priority += '<td>' + link + '</td>'
+                        priority += '<td>' + name + '</td>'
+                        priority += '<td>' + artist + '</td>'
+                        priority += '<td>' + text + '</td>'
+                        priority += '<td>' + flavor + '</tf>'
+                        priority += '</tr>'
+                    }
+                    else{
+                        subBody += '<tr>'
+                        subBody += '<td>' + link +'</td>'
+                        subBody += '<td>' + name +'</td>'
+                        subBody += '<td>' + artist + '</td>'
+                        subBody += '<td>' + text + '</td>'
+                        subBody += '<td>' + flavor + '</td>'
+                        subBody += '</tr>'
+                    }
+                }
+			}
+        tableBody = head + tableBody + priority + subBody
 
-    var tableBody = '<th>' + 'Set' + '</th>'
-    tableBody += '<th>' + 'Card Name' + '</th>'
-    tableBody += '<th>' + 'Artist' + '</th>'
-
-
-				for( var i = 0; i < cards.length; i++){
-						var tableBody += '<tr>';
-						var setID = cards[i]['set_id']
-            setName = setList[setID]
-            tableBody += '<td>' + setName + '</td>';
-						tableBody += '<td>' +cards[i]['name'] + '</td>';
-            tableBody += '<td>' +cards[i]['Artist'] + '</td>';
-						tableBody += '</tr>';
-				}
-
-        var cardTable = document.getElementById('card_table');
+        var cardTable = document.getElementById('card_table')
         if(cardTable){
-            cardTable.innerHTML = tableBody;
+            cardTable.innerHTML = tableBody
         }
     })
     .catch(function(error){
