fd652a7914b7d9f0213f667c0fc21ce66aa0ca45
CGarcia346 <garciac3@carleton.edu>
2018-05-25 21:36:25 -0500

    Updated with javadocs

--- Final/src/sample/StageModel.java
+++ Final/src/sample/StageModel.java
@@ -3,6 +3,9 @@ package sample;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Model of the Stage and characters within it
+ */
 public class StageModel  {
 
     private int playerHP;
@@ -10,22 +13,42 @@ public class StageModel  {
     private ArrayList[][] moveable;
     private boolean gameOver;
 
+    /**
+     * Starts a game for the user
+     */
     public void startNewGame(){
 
     }
 
-    public void getCharacterPos(){
-
+    /**
+     * Short one line description.
+     * @param  character
+     * @return position of the characters
+     */
+    public void getCharacterPos(Player character){
 
     }
-
-    public void updateHPBar(int HP){
+    /**
+     * Will update the HP bar for a character/player that has taken damage
+     * @param  HP
+     * @param  character
+     * @return an update to a character's HP bar
+     */
+    public void updateHPBar(int HP, Player character){
 
     }
-
-    public void moveCharacter(Player player, int row, int column){
+    /**
+     * Moves a character to a different position
+     * @param  character
+     * @param  row
+     * @param  column
+     */
+    public void moveCharacter(Player character, int row, int column){
     }
 
+    /**
+     * Sets up the stage
+     */
     private void initialize(){
 
     }
